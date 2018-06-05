c8eefc8899af28d5c24c1ea7401db7d14ff3ed66
Dean-ZhenYao-Wang <137473325@qq.com>
2018-06-06 02:01:45 +0800

    增加健康检查

---
+++ ID4.IdServer/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ID4.IdServer
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
